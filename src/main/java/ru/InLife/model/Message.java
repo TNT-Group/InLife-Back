274ea69cb3b7c0205bf407a8d8575e298f163450
redbird <redbird8941@gmail.com>
2020-12-03 01:21:43 +0300

    DON'T WORK
    i think problem with liquibase integration, left until better times

--- src/main/java/ru/InLife/model/Message.java
+++ src/main/java/ru/InLife/model/Message.java
@@ -3,6 +3,9 @@ package ru.InLife.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.Date;
 
 @Data
 @Entity
@@ -12,6 +15,9 @@ public class Message {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
+    @NotEmpty(message = "Empty message")
+    @Size(min = 1, max = 255, message = "Wrong size of message")
     public Message() {
     }
 
