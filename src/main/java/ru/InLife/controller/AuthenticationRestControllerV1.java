1e3fb5d02513f84dd25cd8289efdfb5d3ab0bf3b
redbird <redbird8941@gmail.com>
2020-12-03 05:45:43 +0300

    Add rest auth, wait user-logic
    It's not complete, wait user model,service e t.c.

---
+++ src/main/java/ru/InLife/controller/AuthenticationRestControllerV1.java
@@ -0,0 +1,61 @@
+package ru.InLife.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.InLife.security.JwtTokenProvider;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthenticationRestControllerV1 {
+
+    private final AuthenticationManager authenticationManager;
+    private UserRepository userRepository;
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    public AuthenticationRestControllerV1(AuthenticationManager authenticationManager, UserRepository userRepository, JwtTokenProvider jwtTokenProvider) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @PostMapping("/login")
+    //TODO
+    //Wait for user-logic
+    public ResponseEntity<?> authenticate(@RequestBody AuthenticationRequestDTO req){
+        try {
+            String email = req.getEmail();
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(req.getEmail(), req.getPassword()));
+            User user = userRepository.findByEmail(req.getEmail()).orElseThrow(() -> new UsernameNotFoundException("User doesn't exists"));
+            String token = jwtTokenProvider.createToken(req.getEmail(), user.getRole().name());
+            Map<Object, Object> res = new HashMap<>();
+            res.put("email", req.getEmail());
+            res.put("token", token);
+
+            return ResponseEntity.ok(res);
+        } catch (AuthenticationException e) {
+            return new ResponseEntity<>("Invalid email/password combination", HttpStatus.FORBIDDEN)
+        }
+    }
+
+    @PostMapping("/logout")
+    public void logout(HttpServletRequest req, HttpServletResponse res){
+        SecurityContextLogoutHandler securityContextLogoutHandler = new SecurityContextLogoutHandler();
+        securityContextLogoutHandler.logout(req, res, null);
+    }
+}
---
+++ src/main/java/ru/InLife/security/JwtTokenProvider.java
@@ -0,0 +1,70 @@
+package ru.InLife.security;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    //Need User logic
+    @Autowired
+//    private UserDatailsService userDatailsService;
+
+    @Value("${jwt.header}")
+    private String authorizationHeader;
+    @Value("${jwt.secret}")
+    private String secretKey;
+    @Value("${jwt.expiration}")
+    private Long validityInMilliseconds;
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String createToken(String username, String role) {
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put("role", role);
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds * 1000);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return !claimsJws.getBody().getExpiration().before(new Date());
+        } catch (JwtException | IllegalArgumentException e){
+            throw new JwtAuthenticationException("JWT token is expired or invalid", HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    //TODO
+    //Need User-logic
+    public Authentication getAuthentication(String token) {
+        return null;
+    }
+
+    public String getUserName(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public String resolveToken(HttpServletRequest req) {
+        return req.getHeader(authorizationHeader);
+    }
+}
